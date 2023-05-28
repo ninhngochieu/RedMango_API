55462b06c69be2aac164a8bc600ad57cef2d27b0
Ninh Ngoc Hieu <30309069+ninhngochieu@users.noreply.github.com>
2023-05-28 22:43:52 +0700

    Change SQLite to MariaDB

--- RedMango_API/Program.cs
+++ RedMango_API/Program.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
+using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
 using RedMango_API.Data;
 using RedMango_API.Models;
 using RedMango_API.Services;
@@ -12,76 +13,87 @@ using System.Text;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-{
-    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultDbConnection"));
-});
-builder.Services.AddSingleton(u => new BlobServiceClient(
-    builder.Configuration.GetConnectionString("StorageAccount")));
+//builder.Services.AddDbContext<ApplicationDbContext>(options =>
+//{
+//    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultDbConnection"));
+//});
+var connectionString = builder.Configuration.GetConnectionString("DefaultDbConnection");
+builder.Services
+    .AddDbContext<ApplicationDbContext>(
+        options => options.UseMySql(
+            connectionString, ServerVersion.AutoDetect(connectionString)), ServiceLifetime.Scoped); ;
+builder.Services.AddSingleton(u => new BlobServiceClient(builder.Configuration.GetConnectionString("StorageAccount")));
 builder.Services.AddSingleton<IBlobService, BlobService>();
 builder.Services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
 
-builder.Services.Configure<IdentityOptions>(options =>
-{
-    options.Password.RequireDigit = false;
-    options.Password.RequiredLength = 1;
-    options.Password.RequireLowercase = false;
-    options.Password.RequireUppercase = false;
-    options.Password.RequireNonAlphanumeric = false;
-});
+builder.Services
+    .Configure<IdentityOptions>(
+        options =>
+        {
+            options.Password.RequireDigit = false;
+            options.Password.RequiredLength = 1;
+            options.Password.RequireLowercase = false;
+            options.Password.RequireUppercase = false;
+            options.Password.RequireNonAlphanumeric = false;
+        });
 var key = builder.Configuration.GetValue<string>("ApiSettings:Secret");
-builder.Services.AddAuthentication(u =>
-{
-    u.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    u.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-}).AddJwtBearer(u =>
-{
-    u.RequireHttpsMetadata = false;
-    u.SaveToken = true;
-    u.TokenValidationParameters = new TokenValidationParameters
-    {
-        ValidateIssuerSigningKey = true,
-        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(key)),
-        ValidateIssuer = false,
-        ValidateAudience = false
-    };
-});
+builder.Services
+    .AddAuthentication(
+        u =>
+        {
+            u.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            u.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        })
+    .AddJwtBearer(
+        u =>
+        {
+            u.RequireHttpsMetadata = false;
+            u.SaveToken = true;
+            u.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(key)),
+                ValidateIssuer = false,
+                ValidateAudience = false
+            };
+        });
 
 builder.Services.AddCors();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(options =>
-{
-    options.AddSecurityDefinition(JwtBearerDefaults.AuthenticationScheme, new Microsoft.OpenApi.Models.OpenApiSecurityScheme
-    {
-        Description =
-            "JWT Authorization header using the Bearer scheme. \r\n\r\n " +
-            "Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\n" +
-            "Example: \"Bearer 12345abcdef\"",
-        Name = "Authorization",
-        In = ParameterLocation.Header,
-        Scheme = JwtBearerDefaults.AuthenticationScheme
-    });
-    options.AddSecurityRequirement(new OpenApiSecurityRequirement()
-    {
+builder.Services
+    .AddSwaggerGen(
+        options =>
         {
-           new OpenApiSecurityScheme
-            {
-                Reference = new OpenApiReference
-                            {
-                                Type = ReferenceType.SecurityScheme,
-                                Id = "Bearer"
-                            },
-                Scheme = "oauth2",
-                Name = "Bearer",
-                In = ParameterLocation.Header
-            },
-            new List<string>()
-        }
-    });
-});
+            options.AddSecurityDefinition(
+                JwtBearerDefaults.AuthenticationScheme,
+                new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+                {
+                    Description =
+                        "JWT Authorization header using the Bearer scheme. \r\n\r\n " +
+                                    "Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\n" +
+                                    "Example: \"Bearer 12345abcdef\"",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Scheme = JwtBearerDefaults.AuthenticationScheme
+                });
+            options.AddSecurityRequirement(
+                new OpenApiSecurityRequirement()
+                {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = "Bearer" },
+                        Scheme = "oauth2",
+                        Name = "Bearer",
+                        In = ParameterLocation.Header
+                    },
+                    new List<string>()
+                }
+                });
+        });
 
 
 var app = builder.Build();
@@ -89,7 +101,7 @@ MigrateDB(app);
 
 void MigrateDB(WebApplication app)
 {
-    using (var scope = app.Services.CreateScope())
+    using(var scope = app.Services.CreateScope())
     {
         var services = scope.ServiceProvider;
         var dbContext = services.GetRequiredService<ApplicationDbContext>();
@@ -100,17 +112,17 @@ void MigrateDB(WebApplication app)
 // Configure the HTTP request pipeline.
 app.UseSwagger();
 
-if (app.Environment.IsDevelopment())
+if(app.Environment.IsDevelopment())
 {
     app.UseSwaggerUI();
-}
-else
+} else
 {
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
-        c.RoutePrefix = string.Empty;
-    });
+    app.UseSwaggerUI(
+        c =>
+        {
+            c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            c.RoutePrefix = string.Empty;
+        });
 }
 
 app.UseHttpsRedirection();
